b38833d3716a03cf96dfbb1d59e38982f6ab9b12
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-17 12:16:08 -0600

    passing smoke test for ProfilePicture component

---
+++ src/components/ProfilePicture/ProfilePicture.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProfilePicture from './ProfilePicture';
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<ProfilePicture />, div);
+
+    ReactDOM.unmountComponentAtNode(div);
+})
