b8c625cd626cd77313f00ba489b4d8960851ad79
Hendoe <hendo1349@gmail.com>
2020-11-17 09:49:01 -0600

    context/service

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './contexts/userContext';
+import { OtherProvider } from './contexts/otherContext';
 
 ReactDOM.render(
   <BrowserRouter>
-    <App />
+    <UserProvider>
+      <OtherProvider>
+        <App />
+      </OtherProvider>
+    </UserProvider>
   </BrowserRouter>,
   document.getElementById('root')
 );
