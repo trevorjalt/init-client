32848e151f1a894447334b8d23908e8e3dd55179
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-18 16:15:29 -0600

    hexagon in profileTop rendering profile picture and aligned

--- src/components/profileTop/profileTop.js
+++ src/components/profileTop/profileTop.js
@@ -1,5 +1,6 @@
 import React, { useContext } from 'react';
 import UserContext from '../../contexts/userContext';
+import ProfilePic from '../ProfilePic/ProfilePic';
 
 function ProfileTop() {
   const userContext = useContext(UserContext)
@@ -7,23 +8,23 @@ function ProfileTop() {
   let { user } = userContext
   console.log(user.username)
 
-    return (
-      <section> 
-        <div className='hex-wrapper'>
-          <div className='hexagon'></div>
-          <div className='p-wrapper'>
-            <p className='p-filling'>post counter</p>
-            <p className='p-filling'>follower counter</p>
-            <p className='p-filling'>following counter</p>
-          </div>
+  return (
+    <section>
+      <div className='hex-wrapper'>
+        <ProfilePic />
+        <div className='p-wrapper'>
+          <p className='p-filling'>post counter</p>
+          <p className='p-filling'>follower counter</p>
+          <p className='p-filling'>following counter</p>
         </div>
-        <div className='profile-wrapper'>
-          <p className='p-item-top'>{user.username}</p>
-          <p className='p-item-mid'>stack: {user.user_stack}</p>
-          <p className='p-item-bot'>about: {user.about_user}</p>
-        </div>
-      </section>
-    );
+      </div>
+      <div className='profile-wrapper'>
+        <p className='p-item-top'>{user.username}</p>
+        <p className='p-item-mid'>stack: {user.user_stack}</p>
+        <p className='p-item-bot'>about: {user.about_user}</p>
+      </div>
+    </section>
+  );
 };
 
 export default ProfileTop;
