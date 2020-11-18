103dc7992638afd8073468163fe645ba6993d2ad
Ady <ady@gms-macbook-pro.lan>
2020-11-18 11:13:20 -0800

    user account page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -70,6 +70,7 @@ class App extends Component {
                 >
                     {this.renderRoutes()}
                 </main>
+                <Footer />
             </div>
         );
     };
--- src/routes/AccountRoute/AccountRoute.js
+++ src/routes/AccountRoute/AccountRoute.js
@@ -1,13 +1,52 @@
 import React, { Component } from 'react';
 
 class Account extends Component {
+  static defaultProps = {
+    history: {
+      goBack: () => { }
+    }
+}
+
   render() {
-    return (
-      <div>
-        <h1>Le Settings</h1>
-      </div>
-    );
-  };
+      return (
+          <div>
+              <h2>Le Settings</h2>
+              <p></p>
+                  <form>
+                      <fieldset>
+                        <legend>userData</legend>
+                        <label for="fname">fullName:</label>
+                        <span>Adyceum</span>
+                      </fieldset>
+                      <fieldset>
+                        <legend>userData</legend>
+                        <label for="fname">userName:</label>
+                        <span>Adyceum</span>
+                      </fieldset>
+                      <fieldset>
+                        <legend>userData</legend>
+                        <label for="fname">eMail:</label>
+                        <span>AMC@gmail.com</span>
+                      </fieldset>
+                      <fieldset>
+                        <legend>userData</legend>
+                        <label for="fname">fullName:</label>
+                        <span>Adyceum</span>
+                      </fieldset>
+                      <fieldset>
+                        <legend>userData</legend>
+                        <label for="fname">fullName:</label>
+                        <span>Adyceum</span>
+                      </fieldset>
+                      <fieldset>
+                        <legend>userData</legend>
+                        <label for="fname">fullName:</label>
+                        <span>Adyceum</span>
+                      </fieldset>
+                  </form>
+          </div>
+      );
+  }
 };
 
 export default Account;
