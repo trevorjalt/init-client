fbe17d1e4f1e1cbc0293e4f1b5a4a7133008e35a
Ady <ady@gms-macbook-pro.lan>
2020-11-18 12:20:18 -0800

    account info

--- src/routes/AccountRoute/AccountRoute.js
+++ src/routes/AccountRoute/AccountRoute.js
@@ -1,52 +1,56 @@
 import React, { Component } from 'react';
+import UserContext from '../../contexts/userContext';
+import './AccountRoute.css';
 
 class Account extends Component {
-  static defaultProps = {
-    history: {
-      goBack: () => { }
+    static defaultProps = {
+        history: {
+            goBack: () => { }
+        }
     }
-}
 
-  render() {
-      return (
-          <div>
-              <h2>Le Settings</h2>
-              <p></p>
-                  <form>
-                      <fieldset>
-                        <legend>userData</legend>
-                        <label for="fname">fullName:</label>
-                        <span>Adyceum</span>
-                      </fieldset>
-                      <fieldset>
-                        <legend>userData</legend>
-                        <label for="fname">userName:</label>
-                        <span>Adyceum</span>
-                      </fieldset>
-                      <fieldset>
-                        <legend>userData</legend>
-                        <label for="fname">eMail:</label>
-                        <span>AMC@gmail.com</span>
-                      </fieldset>
-                      <fieldset>
-                        <legend>userData</legend>
-                        <label for="fname">fullName:</label>
-                        <span>Adyceum</span>
-                      </fieldset>
-                      <fieldset>
-                        <legend>userData</legend>
-                        <label for="fname">fullName:</label>
-                        <span>Adyceum</span>
-                      </fieldset>
-                      <fieldset>
-                        <legend>userData</legend>
-                        <label for="fname">fullName:</label>
-                        <span>Adyceum</span>
-                      </fieldset>
-                  </form>
-          </div>
-      );
-  }
+    static contextType = UserContext
+
+    // componentDidMount() {}
+  
+
+    render() {
+        const { user } = this.context
+            console.log(user)
+        return (
+            <div className='account-wrapper'>
+                <h2>Account</h2>
+                <p>info</p>
+                    <form className='account-form'>
+                        <fieldset>
+                          <legend><h3>fullName</h3></legend>
+                          <label htmlFor="fname"></label>
+                          <span>Adyceum</span>
+                        </fieldset>
+                        <fieldset>
+                          <legend><h3>userName</h3></legend>
+                          <label htmlFor="fname"></label>
+                          <span>Adyceum</span>
+                        </fieldset>
+                        <fieldset>
+                          <legend><h3>eMail</h3></legend>
+                          <label htmlFor="fname"></label>
+                          <span>AMC@gmail.com</span>
+                        </fieldset>
+                        <fieldset>
+                          <legend><h3>about</h3></legend>
+                          <label htmlFor="fname"></label>
+                          <span>Adyceum</span>
+                        </fieldset>
+                        <fieldset>
+                          <legend><h3>stack</h3></legend>
+                          <label htmlFor="fname"></label>
+                          <span>full-stack</span>
+                        </fieldset>
+                    </form>
+            </div>
+        );
+    }
 };
 
 export default Account;
