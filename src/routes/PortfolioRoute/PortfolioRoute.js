93c613be59bcd7e2d477b04e9718b0d2544637bf
Rachel Reilly <r.a.reill18@gmail.com>
2020-11-20 16:07:33 -0600

    capitalized ProfileTop folder

--- src/routes/PortfolioRoute/PortfolioRoute.js
+++ src/routes/PortfolioRoute/PortfolioRoute.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
-import Gallery from '../../components/gallery/gallery';
-import ProfileTop from '../../components/profileTop/profileTop';
-=======
 import Gallery from '../../components/Gallery/Gallery.YouTuber';
 import ProfileTop from '../../components/ProfileTop/ProfileTop';
->>>>>>> 06a83c823f78cc243bca9fcb47dd7c29659b4832
 import '../../css/Portfolio.css';
 
 class Portfolio extends Component {
