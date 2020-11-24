a1b17232f5f434d9d5affe98c300ed9305052d42
Hendoe <hendo1349@gmail.com>
2020-11-24 16:08:26 -0600

    scroll

--- src/components/Gallery/useBookSearch.js
+++ src/components/Gallery/useBookSearch.js
@@ -6,30 +6,18 @@ import { useEffect, useState } from 'react';
 //people really do be loving this this Axios
 import axios from 'axios';
 
-export default function useBookSerach(query, pageNumber) {
+export default function useBookSearch(observed, pageNumber, limit) {
     //We want to default to loading until our call is complete
     const [loading, setLoading] = useState(true);
     //What do we do about errors?
     const [error, setError] = useState(false);
-    //Until a query is made the books is just an empty array
-    const [books, setBooks] = useState([]);
-    //Supposed we have seen all the books there are to see (numFound)
+    //Until a query is made the results is just an empty array
+    const [results, setResults] = useState([]);
+    //Supposed we have seen all the results there are to see (numFound)
     //How do we know to stop loading more? This guy right here will do just that
     const [hasMore, setHasMore] = useState(false);
 
-    //This is here to handle a bug
-    //Basically everytime the query changes the old return simply appends the new query to it
-    //So the old search never goes away
-    //With this we are defaulting to a blank array everytime the query changes
-    useEffect(() => {
-        setBooks([])
-    }, [query]);
-
-
-    //This is neat... whenever the params change | }, [query, pageNumber]) | then the axios call is made
-    //¿¿Refactor axios to use a Service??
-    //The current URL is just for testing purposes
-    //Our API is going to need Pagination...
+    //This is neat... whenever the param (observer) changes then the axios call is made
     useEffect(() => {
         //Everytime we make a request we have to trigger our loading thingy
         setLoading(true);
@@ -44,26 +32,28 @@ export default function useBookSerach(query, pageNumber) {
         //So the catch way down below will bypass the error instead of ruining your call
         //Then, when you have set your query properly, the call will finally go through
         let cancel
+        console.log('what are we working with', observed, pageNumber, limit)
         axios({
             method: 'GET',
-            url: 'http://openlibrary.org/search.json',
-            params: { q: query, page: pageNumber },
+            url: 'http://localhost:8000/api/post',
+            params: { page: pageNumber, limit: limit },
             cancelToken: axios.CancelToken(c => cancel = c)
         }).then(res => {
+            console.log('what response', res)
             //Here we set our State by combining our Old Books with our New Books
             //As the Sample API is finnicky and clone to duplicates we use the JS built in function Set
             //That create a brand new array of only unique Books
             //*for init this can be used to prevent rendering of duplicate Posts, methinks*
-            setBooks(prevBooks => {
-                return [...new Set([...prevBooks, ...res.data.docs.map(b => b.title)])]
+            setResults(prevResults => {
+                return [...new Set([...prevResults, ...res.data.results.map(p => p)])]
             })
             //We need to determine if there are more books are not
             //As long as we have more than 0 books we know there are more to load
             //When it is 0 we have reached the end of it all
-            setHasMore(res.data.docs.length > 0)
+            setHasMore(res.data.results.length > 0)
             //No reason to Load anything else, right?
             setLoading(false)
-            console.log(res.data)
+            console.log('our axios get', res.data)
             //PROTIP
             //The console log will give you something that look-a like-a this
             //{docs:100, num_found: 67689, numFound:67689, start:0}
@@ -79,6 +69,6 @@ export default function useBookSerach(query, pageNumber) {
             setError(true)
         })
         return () => cancel()
-    }, [query, pageNumber]);
-    return { loading, error, books, hasMore};
+    }, [observed]);
+    return { loading, error, results, hasMore};
 };
